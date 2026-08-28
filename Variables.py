b35dddca94515523594e5140bf670820fc49e16b
swarupa109 <sakshiaeer@gmail.com>
2026-08-28 13:58:25 +0530

    Added varaiable programs
    Corrected variable name 'State' to 'state' in print statement.

---
+++ Variables.py
@@ -0,0 +1,13 @@
+# Python Variables
+
+name = "Swarupa"
+age = 18
+State = "Maharashtra"
+course = "Computer Engineering"
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("state:", state)
+print("Course:", course)
+print("Student:", is_student)
